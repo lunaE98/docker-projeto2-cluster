18fdbffe8f83b8a79c977a145c9d324d86a0d3f8
Luana Silva <103971142+lunaE98@users.noreply.github.com>
2022-11-20 13:41:32 -0300

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
